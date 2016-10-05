524fac512c110b3e37e4e372cae751334a3328da
Fernando A. Sossai <ferpa@sigmanet.com.br>
2016-10-04 21:45:04 -0300

    Tela de Login

---
+++ app/src/main/java/dev42/ironlife/IntroActivity.java
@@ -0,0 +1,13 @@
+package dev42.ironlife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
--- app/src/main/java/dev42/ironlife/MainActivity.java
+++ app/src/main/java/dev42/ironlife/MainActivity.java
@@ -2,6 +2,7 @@ package dev42.ironlife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText login = (EditText)findViewById(R.id.login);
+        EditText senha = (EditText)findViewById(R.id.senha);
+
+//        login.getBackground().setColorFilter();
     }
 }
